62bd17348b18b87b68db09594f008fc4a69457d0
Titus Soporan <titus@tsoporan.com>
2019-03-17 11:59:00 -0400

    Test db connection
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- music_findr.go
+++ music_findr.go
@@ -1,21 +1,59 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/render"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"os"
 	"time"
 )
 
+func getConn() *sql.DB {
+	dbname := os.Getenv("PG_DATABASE")
+	user := os.Getenv("PG_USER")
+	password := os.Getenv("PG_PASSWORD")
+	host := os.Getenv("PG_HOST")
+	port := os.Getenv("PG_PORT")
+
+	connStr := fmt.Sprintf("user=%s dbname=%s password=%s port=%s host=%s sslmode=disable",
+		user,
+		dbname,
+		password,
+		port,
+		host,
+	)
+
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
+
 func apiIndex(w http.ResponseWriter, r *http.Request) {
+	var version string
+
+	db := getConn()
+
+	err := db.QueryRow("SHOW server_version").Scan(&version)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	msg := struct {
-		Status string `json:"status"`
+		Status    string `json:"status"`
+		DBVersion string `json:"dbversion"`
 	}{
 		"OK",
+		version,
 	}
 
 	render.JSON(w, r, msg)
